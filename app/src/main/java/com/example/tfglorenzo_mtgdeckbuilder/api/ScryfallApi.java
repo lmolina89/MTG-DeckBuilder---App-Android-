02e50498f507623919d68a5bc5e1343d5b58835b
Lorenzo Molina Moreno <lmolinamoreno@hotmail.com>
2023-05-06 17:02:41 +0200

    implementada interfaz scryfall api

--- app/src/main/java/com/example/tfglorenzo_mtgdeckbuilder/api/ScryfallApi.java
+++ app/src/main/java/com/example/tfglorenzo_mtgdeckbuilder/api/ScryfallApi.java
@@ -1,4 +1,12 @@
 package com.example.tfglorenzo_mtgdeckbuilder.api;
 
+import com.example.tfglorenzo_mtgdeckbuilder.models.api.ResponseFromApi;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ScryfallApi {
+    @GET("cards/search")
+    Call<ResponseFromApi> searchCards(@Query("q") String query);
 }
